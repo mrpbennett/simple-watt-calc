92ce5e105c9277628a5fe3dbb674ac1a0b5f9bf1
Paul Bennett <pbennett.uk@outlook.com>
2019-03-07 11:30:13 +0000

    zwift added but need to sort the logic

--- main.js
+++ main.js
@@ -28,86 +28,105 @@ $(document).ready(function () {
 
         // 5s ranking 
         if (pwr5s < 12.35 && pwr5s > 10.17 ) {
-            $('#class-5s').text('Untrained')
+            $('#class-5s').text('Untrained - Non Racer')
         } else if(pwr5s < 13.98 && pwr5s > 11.80) {
-            $('#class-5s').text('Fair')
+            $('#class-5s').text('Fair - Cat 5')
         } else if (pwr5s < 15.61 && pwr5s > 13.71) {
-            $('#class-5s').text('Moderate')
+            $('#class-5s').text('Moderate - Cat 4')
         } else if (pwr5s < 17.24 && pwr5s > 15.07) {
-            $('#class-5s').text('Good')
+            $('#class-5s').text('Good - Cat 3')
         } else if (pwr5s < 19.15 && pwr5s > 16.97) {
-            $('#class-5s').text('Very Good')
+            $('#class-5s').text('Very Good - Cat 3 - Cat 2')
         } else if (pwr5s < 20.78 && pwr5s > 18.60) {
-            $('#class-5s').text('Excellent')
+            $('#class-5s').text('Excellent - Cat 1')
         } else if (pwr5s < 22.41 && pwr5s > 20.23) {
-            $('#class-5s').text('Exceptional')
+            $('#class-5s').text('Exceptional - Domestic Pro')
         } else if (pwr5s < 24.04 && pwr5s > 21.86) {
-            $('#class-5s').text('World Class')
+            $('#class-5s').text('World Class - International Pro')
         } else {
             $('#class-1min').text('You need to work on this!')
         }
 
         // 1m ranking 
         if (pwr1min < 6.56 && pwr1min > 5.64) {
-            $('#class-1min').text('Untrained')
+            $('#class-1min').text('Untrained - Non Racer')
         } else if (pwr1min < 7.25 && pwr1min > 6.33) {
-            $('#class-1min').text('Fair')
+            $('#class-1min').text('Fair - Cat 5')
         } else if (pwr1min < 7.94 && pwr1min > 7.02) {
-            $('#class-1min').text('Moderate')
+            $('#class-1min').text('Moderate - Cat 4')
         } else if (pwr1min < 8.63 && pwr1min > 7.71) {
-            $('#class-1min').text('Good')
+            $('#class-1min').text('Good - Cat 3')
         } else if (pwr1min < 9.43 && pwr1min > 8.51) {
-            $('#class-1min').text('Very Good')
+            $('#class-1min').text('Very Good - Cat 3 - Cat 2')
         } else if (pwr1min < 10.12 && pwr1min > 9.20) {
-            $('#class-1min').text('Excellent')
+            $('#class-1min').text('Excellent - Cat 1')
         } else if (pwr1min < 10.81 && pwr1min > 9.81) {
-            $('#class-1min').text('Exceptional')
+            $('#class-1min').text('Exceptional - Domestic Pro')
         } else if (pwr1min < 11.50 && pwr1min > 10.58) {
-            $('#class-1min').text('World Class')
+            $('#class-1min').text('World Class - International Pro')
         } else {
             $('#class-1min').text('You need to work on this!')
         }
 
         // 5min ranking
         if (pwr5min < 3.15 && pwr5min > 2.33) {
-            $('#class-5min').text('Untrained')
+            $('#class-5min').text('Untrained - Non Racer')
         } else if (pwr5min < 3.77 && pwr5min > 2.95) {
-            $('#class-5min').text('Fair')
+            $('#class-5min').text('Fair - Cat 5')
         } else if (pwr5min < 4.39 && pwr5min > 3.57) {
-            $('#class-5min').text('Moderate')
+            $('#class-5min').text('Moderate - Cat 4')
         } else if (pwr5min < 5.01 && pwr5min > 4.19) {
-            $('#class-5min').text('Good')
+            $('#class-5min').text('Good - Cat 3')
         } else if (pwr5min < 5.74 && pwr5min > 4.91) {
-            $('#class-5min').text('Very Good')
+            $('#class-5min').text('Very Good - Cat 3 - Cat 2')
         } else if (pwr5min < 6.36 && pwr5min > 5.53) {
-            $('#class-5min').text('Excellent')
+            $('#class-5min').text('Excellent - Cat 1')
         } else if (pwr5min < 6.98 && pwr5min > 6.15) {
-            $('#class-5min').text('Exceptional')
+            $('#class-5min').text('Exceptional - Domestic Pro')
         } else if (pwr5min < 7.60 && pwr5min > 6.77) {
-            $('#class-5min').text('World Class')
+            $('#class-5min').text('World Class - International Pro')
         } else {
             $('#class-1min').text('You need to work on this!')
         }
 
         //ftp ranking
         if (pwrftp < 2.58 && pwrftp > 1.86) {
-            $('#class-ftp').text('Untrained')
+            $('#class-ftp').text('Untrained - Non Racer')
         } else if (pwrftp < 3.11 && pwrftp > 2.40) {
-            $('#class-ftp').text('Fair')
+            $('#class-ftp').text('Fair - Cat 5')
         } else if (pwrftp < 3.64 && pwrftp > 2.93) {
-            $('#class-ftp').text('Moderate')
+            $('#class-ftp').text('Moderate - Cat 4')
         } else if (pwrftp < 4.18 && pwrftp > 3.47) {
-            $('#class-ftp').text('Good')
+            $('#class-ftp').text('Good - Cat 3')
         } else if (pwrftp < 4.80 && pwrftp > 4.09) {
-            $('#class-ftp').text('Very Good')
+            $('#class-ftp').text('Very Good - Cat 3 - Cat 2')
         } else if (pwrftp < 5.33 && pwrftp > 4.62) {
-            $('#class-ftp').text('Excellent')
+            $('#class-ftp').text('Excellent - Cat 1')
         } else if (pwrftp < 5.87 && pwrftp > 5.15) {
-            $('#class-ftp').text('Exceptional')
+            $('#class-ftp').text('Exceptional - Domestic Pro')
         } else if (pwrftp < 6.40 && pwrftp > 5.69) {
-            $('#class-ftp').text('World Class')
+            $('#class-ftp').text('World Class - International Pro')
         } else {
             $('#class-1min').text('You need to work on this!')
+        }
+
+        // zwift racing
+        if (pwrftp < 5.00 && pwrftp > 4.00) {
+            $('.zwift-cat').html('A')
+            $('.zwift-color').css('background-color: #FA472C;')
+            console.log('Zwift Cat A')
+        } else if (pwrftp < 4.00 && pwrftp > 3.20) {
+            $('.zwift-cat').html('B')
+            $('.zwift-color').css('background-color: #5BC252;')
+            console.log('Zwift Cat B')
+        } else if (pwrftp < 3.10 && pwrftp > 2.50) {
+            $('.zwift-cat').html('C')
+            $('.zwift-color').css('background-color: #44BFE8;')
+            console.log('Zwift Cat C')
+        } else if (pwrftp < 2.40 && pwrftp > 1.00) {
+            $('.zwift-cat').html('D')
+            $('.zwift-color').css('background-color: #FCD030;')
+            console.log('Zwift Cat D')
         }
         
 
